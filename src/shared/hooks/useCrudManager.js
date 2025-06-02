b364b4d1d9bbaddfa4322be208462f7562168586
LAPTOP-V0431P9B\HOME <kalelcarlosmartinez@gmail.com>
2025-06-02 12:12:41 -0400

    removing logs

--- src/shared/hooks/useCrudManager.js
+++ src/shared/hooks/useCrudManager.js
@@ -9,8 +9,6 @@ export function useCrudManager(service, handleError, t) {
     setLoading(true);
     try {
       const data = await service.getAll();
-      console.log('data');
-      console.log(data);
       if (Array.isArray(data)) {
         setItems(data);
       } else {
