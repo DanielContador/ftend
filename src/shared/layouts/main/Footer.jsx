f0dbe6d6881e160389e57989b6b6c0315f579881
LAPTOP-V0431P9B\HOME <kalelcarlosmartinez@gmail.com>
2025-06-04 08:08:44 -0400

    some fixes in the main layout component

---
+++ src/shared/layouts/main/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    © 2025 Digital Learning. Todos los derechos reservados.
+  </footer>
+);
+
+export default Footer;
--- src/shared/layouts/main/Layout.jsx
+++ src/shared/layouts/main/Layout.jsx
@@ -1,10 +1,14 @@
 import styles from "./Layout.module.css";
 import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => (
   <div className={styles.root}>
     <Sidebar />
-    <div className={styles.content}>{children}</div>
+    <div className={styles.content}>
+      {children}
+      <Footer />
+    </div>
   </div>
 );
 
